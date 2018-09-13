553167f94492a481403cd34a66d8360250073baa
ReiJr <reinaldo.isj@hotmail.com>
2018-09-13 11:42:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,19 @@
-'''import urllib
+#rom __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
 import json
+
 import os
 from flask import Flask
 from flask import request
 from flask import make_response
 
-app = Flask(__name__)'''
-'''
-@app.route("/")
-def hello():
-        return "Hello from Python!"
+# Flask app should start in global layout
+app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-        req = request.get_json(silent=True, force=True)
-        print ("Request:")
-        print (json.dumps(req, indent=4))
-        res = makeWehookResult(req)
-        res = json.dumps(res, indent=4)
-        print (res)
-        r = make_response(res)
-        r.header['Content-Type'] = 'application/json'
-        return r
-
-def makeWebhookResult(req):
-        if req.get("result").get("action")!= "portofaz":
-                return {}
-        result = req.get("queryResult")
-        parameters = result.get("parameters")
-        name = parameters.get("servico")
-        #bank = {'Federal Bank':'6.7%', 'Andhra Bank':'6.85%', 'Bandhan Bank':'7.15%'}
-        speech = "Olá, a Porto Faz consegue ajudar com " + name + ",quer mais detalhe que sobre o serviço?"
-        print ("Response: ")
-        print (speech)
-        return {
-                "fulfillmentText": speech,
-                "source": "portofaz"
-                }
-                
-                
-
-'''
 @app.route("/")
 def hello():
         return "Hello from Python!"
@@ -136,5 +109,19 @@ def static_reply():
 
 
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+'''
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
